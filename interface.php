60b513d72113ca2fcf8e857b03aa1acda1ea4b0d
muriukiMartin <64072957+muriukiMartin@users.noreply.github.com>
2020-10-28 13:26:27 +0300

    Interface

---
+++ interface.php
@@ -0,0 +1,13 @@
+<?php
+interface Account{
+    public function register($pdo);
+    public function login($pdo);
+    public function changePassword($pdo);
+    public function logout($pdo);
+}
+
+
+
+
+
+?>
